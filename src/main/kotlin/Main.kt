b65aa941d5bc6409cb79bf7e8626cda18ac0b4c5
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-22 15:59:44 +0300

    implement functions task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
 package org.example
 
-import org.example.while_conditions.WhileConditionsManager
-
 fun main() {
-   WhileConditionsManager().execute()
+    printName()
+}
+
+fun greeting(message: String = "greeting ahmed ") {
+    println(message)
+}
+
+var anyValue: Any = 10
+fun casting() {
+    val intValue: Int = anyValue as Int
+    println(intValue)
+}
+
+
+fun checkingString() {
+    var nullableVar: String? = null
+    nullableVar?.let { println(it.length) }
+        ?: run {
+            nullableVar = "ahmed"
+            println(nullableVar!!.length)
+        }
+}
+
+fun printName() {
+    val readName = readlnOrNull()
+    val nullableVar: String = readName ?: "Guest"
+    println(nullableVar)
 }
